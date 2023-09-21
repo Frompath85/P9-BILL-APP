569986901c38db940b1930a761579649cb008701
baya laadjel <bata_cher@hotmail.fr>
2023-09-21 15:06:30 +0200

    ajouter .off().on() L150

--- Billed-app-FR-Front/src/containers/Dashboard.js
+++ Billed-app-FR-Front/src/containers/Dashboard.js
@@ -95,7 +95,7 @@ export default class {
       $(`#open-bill${bill.id}`).css({ background: '#2A2B35' })
       $('.dashboard-right-container div').html(DashboardFormUI(bill))
       $('.vertical-navbar').css({ height: '150vh' })
-      this.counter ++
+      this.counter ++  // sinon supprimer le counter
     } else {
       $(`#open-bill${bill.id}`).css({ background: '#0D5AE5' })
 
@@ -146,7 +146,9 @@ export default class {
     }
 
     bills.forEach(bill => {
-      $(`#open-bill${bill.id}`).click((e) => this.handleEditTicket(e, bill, bills))
+        /* a chaque click on enleve et on rattache l'evenement */
+        $(`#open-bill${bill.id}`).off().on().click((e) => this.handleEditTicket(e, bill, bills))
+     // $(`#open-bill${bill.id}`).click((e) => this.handleEditTicket(e, bill, bills))
     })
 
     return bills
